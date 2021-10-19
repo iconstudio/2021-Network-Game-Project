d002bb25083d1f48803c63d4ed6854ac9ef68887
Iconer <yoyofa2@hotmail.com>
2021-10-19 15:15:36 +0900

    UDP client is modified.

---
+++ ws-udp-client/udp_Client.cpp
@@ -0,0 +1,37 @@
+#pragma comment(lib, "ws2_32")
+#define _CRT_SECURE_NO_WARNINGS
+#define _WINSOCK_DEPRECATED_NO_WARNINGS
+
+#define SERVER_IP "127.0.0.1"
+#define SERVER_PORT 9000
+#define BUFFER_SIZE 512
+
+#include <WinSock2.h>
+#include <stdio.h>
+using namespace std;
+
+void err_quit(const char* msg) {
+	LPVOID lpMSGBuffer;
+
+	int error_code = WSAGetLastError();
+	FormatMessage(FORMAT_MESSAGE_ALLOCATE_BUFFER | FORMAT_MESSAGE_FROM_SYSTEM, NULL, error_code,
+				  MAKELANGID(LANG_NEUTRAL, SUBLANG_DEFAULT), (LPTSTR)(&lpMSGBuffer), 0, NULL);
+
+	MessageBox(NULL, (LPTSTR)(lpMSGBuffer), msg, MB_ICONERROR);
+
+	LocalFree(lpMSGBuffer);
+
+	exit(1);
+}
+
+void err_display(const char* msg) {
+	LPVOID lpMSGBuffer;
+
+	int error_code = WSAGetLastError();
+	FormatMessage(FORMAT_MESSAGE_ALLOCATE_BUFFER | FORMAT_MESSAGE_FROM_SYSTEM, NULL, error_code,
+				  MAKELANGID(LANG_NEUTRAL, SUBLANG_DEFAULT), (LPTSTR)(&lpMSGBuffer), 0, NULL);
+
+	cout << msg << " - " << (char*)(lpMSGBuffer);
+
+	LocalFree(lpMSGBuffer);
+}
