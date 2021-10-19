f527861720793efdbe5e6d1167dff71f7aebee91
Iconer <yoyofa2@hotmail.com>
2021-10-19 15:15:52 +0900

    Error msgs for UDP server

--- ws-udp-server/udp_Server.cpp
+++ ws-udp-server/udp_Server.cpp
@@ -2,15 +2,10 @@
 #define _CRT_SECURE_NO_WARNINGS
 #define _WINSOCK_DEPRECATED_NO_WARNINGS
 #define SERVER_PORT 9000
-#define PATH_LENGTH 1024
 #define BUFFER_SIZE 512
-#define THREADS_NUM 2
 
 #include <WinSock2.h>
 #include <stdio.h>
-#include <iostream>
-#include <fstream>
-#include <vector>
 using namespace std;
 
 void err_quit(const char* msg);
@@ -65,5 +60,31 @@ int main(void) {
 	}
 
 	closesocket(listener);
+}
+
+void err_quit(const char* msg) {
+	LPVOID lpMSGBuffer;
+
+	int error_code = WSAGetLastError();
+	FormatMessage(FORMAT_MESSAGE_ALLOCATE_BUFFER | FORMAT_MESSAGE_FROM_SYSTEM, NULL, error_code,
+				  MAKELANGID(LANG_NEUTRAL, SUBLANG_DEFAULT), (LPTSTR)(&lpMSGBuffer), 0, NULL);
+
+	MessageBox(NULL, (LPTSTR)(lpMSGBuffer), msg, MB_ICONERROR);
+
+	LocalFree(lpMSGBuffer);
+
+	exit(1);
+}
+
+void err_display(const char* msg) {
+	LPVOID lpMSGBuffer;
+
+	int error_code = WSAGetLastError();
+	FormatMessage(FORMAT_MESSAGE_ALLOCATE_BUFFER | FORMAT_MESSAGE_FROM_SYSTEM, NULL, error_code,
+				  MAKELANGID(LANG_NEUTRAL, SUBLANG_DEFAULT), (LPTSTR)(&lpMSGBuffer), 0, NULL);
+
+	cout << msg << " - " << (char*)(lpMSGBuffer);
+
+	LocalFree(lpMSGBuffer);
 }
 
