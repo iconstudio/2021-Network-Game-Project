4877c6350a4498da55299c343c09e5bfde526848
Iconer <yoyofa2@hotmail.com>
2021-10-28 00:24:52 +0900

    Update udp_Client.cpp

--- ws-udp-client/udp_Client.cpp
+++ ws-udp-client/udp_Client.cpp
@@ -12,6 +12,49 @@
 using namespace std;
 
 int main() {
+	WSADATA wsa;
+
+	if (WSAStartup(MAKEWORD(2, 2), &wsa) != 0) return 1;
+
+	SOCKET client_socket = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP);
+	if (INVALID_SOCKET == client_socket) err_quit("socket");
+
+	SOCKADDR_IN server_address;
+	ZeroMemory(&server_address, sizeof(server_address));
+	server_address.sin_family = AF_INET;
+	server_address.sin_addr.s_addr = inet_addr(SERVER_IP);
+	server_address.sin_port = htons(SERVER_PORT);
+
+	char buffer[BUFFER_SIZE + 1];
+
+	while (true) {
+		ZeroMemory(buffer, sizeof(buffer));
+
+		int result = sendto(client_socket, buffer, BUFFER_SIZE, 0
+							, (SOCKADDR*)(&server_address), sizeof(server_address));
+		if (SOCKET_ERROR == result) {
+			err_display("sendto");
+			continue;
+		}
+
+		SOCKADDR_IN peer_address;
+		int peer_length = sizeof(peer_address);
+
+		ZeroMemory(&peer_address, peer_length);
+		server_address.sin_family = AF_INET;
+		server_address.sin_addr.s_addr = inet_addr(SERVER_IP);
+		server_address.sin_port = htons(SERVER_PORT);
+
+		result = recvfrom(client_socket, buffer, BUFFER_SIZE, 0
+						  , (SOCKADDR*)(&peer_address), &peer_length);
+		if (SOCKET_ERROR == result) {
+			err_display("recvfrom");
+			continue;
+		}
+	}
+
+	closesocket(client_socket);
+
 	return 0;
 }
 
