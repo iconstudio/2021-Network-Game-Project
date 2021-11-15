959ccdc55c8a99eb8ee17db7b2ccce7de8cb4045
Iconer <yoyofa2@hotmail.com>
2021-11-15 21:28:46 +0900

    GUI 클라이언트 구현

--- ws-gui-client/ws-gui-client.cpp
+++ ws-gui-client/ws-gui-client.cpp
@@ -1,180 +1,176 @@
-﻿// ws-gui-client.cpp : 애플리케이션에 대한 진입점을 정의합니다.
-//
+﻿#pragma comment(lib, "ws2_32")
+#define _CRT_SECURE_NO_WARNINGS
+#define _WINSOCK_DEPRECATED_NO_WARNINGS
 
-#include "framework.h"
-#include "ws-gui-client.h"
+#include <WinSock2.h>
+#include <commctrl.h>
+#include <stdlib.h>
+#include <iostream>
+#include <fstream>
+using namespace std;
 
-#define MAX_LOADSTRING 100
+#include "Resource.h"
 
-// 전역 변수:
-HINSTANCE hInst;                                // 현재 인스턴스입니다.
-WCHAR szTitle[MAX_LOADSTRING];                  // 제목 표시줄 텍스트입니다.
-WCHAR szWindowClass[MAX_LOADSTRING];            // 기본 창 클래스 이름입니다.
+//#define SERVER_IP "172.30.1.11"
+#define SERVER_IP "127.0.0.1"
+#define SERVER_PORT 9000
+#define CLIENT_MAX_NUMBER 2
+#define BUFF_SIZE 1024
 
-// 이 코드 모듈에 포함된 함수의 선언을 전달합니다:
-ATOM                MyRegisterClass(HINSTANCE hInstance);
-BOOL                InitInstance(HINSTANCE, int);
-LRESULT CALLBACK    WndProc(HWND, UINT, WPARAM, LPARAM);
-INT_PTR CALLBACK    About(HWND, UINT, WPARAM, LPARAM);
+
+SOCKET my_socket;
+
+HWND my_transfer_status;
+HWND my_filename_label;
+bool file_is_ready = false;
+
+DWORD WINAPI FileTransferProcess(LPVOID lpparameter);
+INT_PTR CALLBACK DlgProcedure(HWND, UINT, WPARAM, LPARAM);
+
+void ErrorAbort(const char* msg);
+void ErrorDisplay(const char* msg);
 
 int APIENTRY wWinMain(_In_ HINSTANCE hInstance,
-                     _In_opt_ HINSTANCE hPrevInstance,
-                     _In_ LPWSTR    lpCmdLine,
-                     _In_ int       nCmdShow)
-{
-    UNREFERENCED_PARAMETER(hPrevInstance);
-    UNREFERENCED_PARAMETER(lpCmdLine);
+					 _In_opt_ HINSTANCE hPrevInstance,
+					 _In_ LPWSTR lpCmdLine,
+					 _In_ int nCmdShow) {
+	CreateThread(NULL, 0, FileTransferProcess, NULL, 0, NULL);
+	
+	DialogBox(hInstance, MAKEINTRESOURCE(IDD_DIALOG1), NULL, DlgProcedure);
 
-    // TODO: 여기에 코드를 입력합니다.
-
-    // 전역 문자열을 초기화합니다.
-    LoadStringW(hInstance, IDS_APP_TITLE, szTitle, MAX_LOADSTRING);
-    LoadStringW(hInstance, IDC_WSGUICLIENT, szWindowClass, MAX_LOADSTRING);
-    MyRegisterClass(hInstance);
-
-    // 애플리케이션 초기화를 수행합니다:
-    if (!InitInstance (hInstance, nCmdShow))
-    {
-        return FALSE;
-    }
-
-    HACCEL hAccelTable = LoadAccelerators(hInstance, MAKEINTRESOURCE(IDC_WSGUICLIENT));
-
-    MSG msg;
-
-    // 기본 메시지 루프입니다:
-    while (GetMessage(&msg, nullptr, 0, 0))
-    {
-        if (!TranslateAccelerator(msg.hwnd, hAccelTable, &msg))
-        {
-            TranslateMessage(&msg);
-            DispatchMessage(&msg);
-        }
-    }
-
-    return (int) msg.wParam;
+	closesocket(my_socket);
+	WSACleanup();
+	return 0;
 }
 
+INT_PTR CALLBACK DlgProcedure(HWND hDlg, UINT message, WPARAM wParam, LPARAM lParam) {
+	switch (message) {
+		case WM_INITDIALOG:
+		{
+			my_transfer_status = GetDlgItem(hDlg, IDL_CONNECT);
+			my_filename_label = GetDlgItem(hDlg, IDL_FILENAME);
 
+			SetWindowText(my_transfer_status, TEXT("연결 없음"));
+			SetWindowText(my_filename_label, TEXT("불러온 파일 없음"));
 
-//
-//  함수: MyRegisterClass()
-//
-//  용도: 창 클래스를 등록합니다.
-//
-ATOM MyRegisterClass(HINSTANCE hInstance)
-{
-    WNDCLASSEXW wcex;
+			return TRUE;
+		}
 
-    wcex.cbSize = sizeof(WNDCLASSEX);
+		case WM_COMMAND:
+		{
+			switch (wParam) {
+				case IDC_FILEOPENBUTTON: // 파일 열기
+				{
+					OPENFILENAME my_open_dialog;
+					ZeroMemory(&my_open_dialog, sizeof(LPOPENFILENAME));
 
-    wcex.style          = CS_HREDRAW | CS_VREDRAW;
-    wcex.lpfnWndProc    = WndProc;
-    wcex.cbClsExtra     = 0;
-    wcex.cbWndExtra     = 0;
-    wcex.hInstance      = hInstance;
-    wcex.hIcon          = LoadIcon(hInstance, MAKEINTRESOURCE(IDI_WSGUICLIENT));
-    wcex.hCursor        = LoadCursor(nullptr, IDC_ARROW);
-    wcex.hbrBackground  = (HBRUSH)(COLOR_WINDOW+1);
-    wcex.lpszMenuName   = MAKEINTRESOURCEW(IDC_WSGUICLIENT);
-    wcex.lpszClassName  = szWindowClass;
-    wcex.hIconSm        = LoadIcon(wcex.hInstance, MAKEINTRESOURCE(IDI_SMALL));
+					my_open_dialog.hwndOwner = hDlg;
+					
+					BOOL result = GetOpenFileName(&my_open_dialog);
+					if (result) {
+						file_is_ready = true;
+					}
+				}
+				break;
 
-    return RegisterClassExW(&wcex);
+				case IDC_SENDBUTTON:
+				{
+					if (file_is_ready) {
+						char file_path[BUFF_SIZE];
+
+						scanf_s("%s", &file_path, BUFF_SIZE);
+						file_path[BUFF_SIZE - 1] = '\0';
+
+						long file_size = 0;
+						char* file_buffer = nullptr;
+						FILE* myfile = fopen(file_path, "rb");
+						if (myfile) {
+							fseek(myfile, 0, SEEK_END);
+							file_size = ftell(myfile);
+							fseek(myfile, 0, SEEK_SET);
+
+							file_buffer = new char[file_size];
+							ZeroMemory(file_buffer, file_size);
+							fread(file_buffer, file_size, 1, myfile);
+							fclose(myfile);
+						} else {
+							//err_quit("fopen");
+						}
+
+						//send_packet(my_socket, file_path, strlen(file_path));
+						//send_packet(my_socket, (char*)(file_buffer), file_size);
+					}
+				}
+				break;
+
+				case IDCLOSE:
+				{
+					EndDialog(hDlg, LOWORD(wParam));
+					return TRUE;
+				}
+			}
+		}
+		break;
+
+		case WM_CLOSE:
+		{
+			EndDialog(hDlg, 0);
+			return TRUE;
+		}
+		break;
+	}
+	return FALSE;
 }
 
-//
-//   함수: InitInstance(HINSTANCE, int)
-//
-//   용도: 인스턴스 핸들을 저장하고 주 창을 만듭니다.
-//
-//   주석:
-//
-//        이 함수를 통해 인스턴스 핸들을 전역 변수에 저장하고
-//        주 프로그램 창을 만든 다음 표시합니다.
-//
-BOOL InitInstance(HINSTANCE hInstance, int nCmdShow)
-{
-   hInst = hInstance; // 인스턴스 핸들을 전역 변수에 저장합니다.
+DWORD WINAPI FileTransferProcess(LPVOID lpparameter) {
+	WSADATA wsa;
+	if (WSAStartup(MAKEWORD(2, 2), &wsa) != 0) return 1;
 
-   HWND hWnd = CreateWindowW(szWindowClass, szTitle, WS_OVERLAPPEDWINDOW,
-      CW_USEDEFAULT, 0, CW_USEDEFAULT, 0, nullptr, nullptr, hInstance, nullptr);
+	my_socket = socket(AF_INET, SOCK_STREAM, 0);
+	if (INVALID_SOCKET == my_socket) ErrorAbort("socket");
 
-   if (!hWnd)
-   {
-      return FALSE;
-   }
+	SOCKADDR_IN server_address;
+	int addr_size = sizeof(server_address);
 
-   ShowWindow(hWnd, nCmdShow);
-   UpdateWindow(hWnd);
+	ZeroMemory(&server_address, addr_size);
+	server_address.sin_family = AF_INET;
+	server_address.sin_addr.s_addr = inet_addr(SERVER_IP);
+	server_address.sin_port = htons(SERVER_PORT);
 
-   return TRUE;
+	if (SOCKET_ERROR == connect(my_socket, (SOCKADDR*)(&server_address), addr_size))
+		ErrorAbort("connect");
+
+	char connect_text[64];
+	ZeroMemory(connect_text, 64);
+
+	wsprintf(connect_text, TEXT("서버와 연결됨 - %s"), SERVER_IP);
+	SetWindowText(my_transfer_status, connect_text);
+
+	return 0;
 }
 
-//
-//  함수: WndProc(HWND, UINT, WPARAM, LPARAM)
-//
-//  용도: 주 창의 메시지를 처리합니다.
-//
-//  WM_COMMAND  - 애플리케이션 메뉴를 처리합니다.
-//  WM_PAINT    - 주 창을 그립니다.
-//  WM_DESTROY  - 종료 메시지를 게시하고 반환합니다.
-//
-//
-LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
-{
-    switch (message)
-    {
-    case WM_COMMAND:
-        {
-            int wmId = LOWORD(wParam);
-            // 메뉴 선택을 구문 분석합니다:
-            switch (wmId)
-            {
-            case IDM_ABOUT:
-                DialogBox(hInst, MAKEINTRESOURCE(IDD_ABOUTBOX), hWnd, About);
-                break;
-            case IDM_EXIT:
-                DestroyWindow(hWnd);
-                break;
-            default:
-                return DefWindowProc(hWnd, message, wParam, lParam);
-            }
-        }
-        break;
-    case WM_PAINT:
-        {
-            PAINTSTRUCT ps;
-            HDC hdc = BeginPaint(hWnd, &ps);
-            // TODO: 여기에 hdc를 사용하는 그리기 코드를 추가합니다...
-            EndPaint(hWnd, &ps);
-        }
-        break;
-    case WM_DESTROY:
-        PostQuitMessage(0);
-        break;
-    default:
-        return DefWindowProc(hWnd, message, wParam, lParam);
-    }
-    return 0;
+void ErrorAbort(const char* msg) {
+	LPVOID lpMSGBuffer;
+
+	int error_code = WSAGetLastError();
+	FormatMessage(FORMAT_MESSAGE_ALLOCATE_BUFFER | FORMAT_MESSAGE_FROM_SYSTEM, NULL, error_code,
+				  MAKELANGID(LANG_NEUTRAL, SUBLANG_DEFAULT), (LPTSTR)(&lpMSGBuffer), 0, NULL);
+
+	MessageBox(NULL, (LPTSTR)(lpMSGBuffer), msg, MB_ICONERROR);
+
+	LocalFree(lpMSGBuffer);
+
+	exit(1);
 }
 
-// 정보 대화 상자의 메시지 처리기입니다.
-INT_PTR CALLBACK About(HWND hDlg, UINT message, WPARAM wParam, LPARAM lParam)
-{
-    UNREFERENCED_PARAMETER(lParam);
-    switch (message)
-    {
-    case WM_INITDIALOG:
-        return (INT_PTR)TRUE;
+void ErrorDisplay(const char* msg) {
+	LPVOID lpMSGBuffer;
 
-    case WM_COMMAND:
-        if (LOWORD(wParam) == IDOK || LOWORD(wParam) == IDCANCEL)
-        {
-            EndDialog(hDlg, LOWORD(wParam));
-            return (INT_PTR)TRUE;
-        }
-        break;
-    }
-    return (INT_PTR)FALSE;
+	int error_code = WSAGetLastError();
+	FormatMessage(FORMAT_MESSAGE_ALLOCATE_BUFFER | FORMAT_MESSAGE_FROM_SYSTEM, NULL, error_code,
+				  MAKELANGID(LANG_NEUTRAL, SUBLANG_DEFAULT), (LPTSTR)(&lpMSGBuffer), 0, NULL);
+
+	//cout << msg << " - " << (char*)(lpMSGBuffer);
+
+	LocalFree(lpMSGBuffer);
 }
